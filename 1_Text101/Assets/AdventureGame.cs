b74d67c5f65025bf7c8ad8e80d46d87d38346977
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-06 10:12:02 +0700

    state of game

---
+++ 1_Text101/Assets/AdventureGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingText;
+    
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingText;
+        textComponent.text = state.GetStateStory(); ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageStates();
+    }
+
+    private void ManageStates()
+    {
+        var nextState = state.GetNextState();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextState[1];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
